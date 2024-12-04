92ed5e834e0e6c84d23059c39aad8c910216b8fa
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2024-12-04 18:11:15 +0800

    update

---
+++ lua/custom/keymap.lua
@@ -0,0 +1,16 @@
+local telescope = require 'telescope.builtin'
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew' })
+
+vim.keymap.set('n', '<leader>pf', function()
+  telescope.find_files {
+    cwd = vim.fn.getcwd(), -- Ensure the search is scoped to the current working directory
+    hidden = true,
+  }
+end, { desc = '[P]roject [F]iles' })
+
+vim.keymap.set('n', '<leader>gf', function()
+  telescope.git_files {
+    show_untracked = true,
+  }
+end, { desc = '[G]it [F]iles' })
---
+++ lua/custom/set.lua
@@ -0,0 +1,25 @@
+#vim.opt.guicursor = '' -- Disable GUI cursor for a consistent terminal appearance
+
+vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.number = true -- Show absolute line number for the current line
+
+vim.opt.tabstop = 4 -- Number of spaces for a tab
+vim.opt.softtabstop = 4 -- Number of spaces for a tab in insert mode
+vim.opt.shiftwidth = 4 -- Number of spaces for each indentation level
+vim.opt.expandtab = true -- Convert tabs to spaces
+
+vim.opt.smartindent = true -- Enable smart indentation based on syntax
+
+vim.opt.wrap = false -- Disable line wrapping
+
+vim.opt.hlsearch = false -- Disable search result highlighting
+vim.opt.incsearch = true -- Enable incremental search
+
+vim.opt.termguicolors = true -- Enable true color support in the terminal
+
+vim.opt.scrolloff = 8 -- Keep 8 lines visible above and below the cursor
+vim.opt.signcolumn = 'yes' -- Always show the sign column to prevent text shifting
+
+vim.opt.updatetime = 50 -- Reduce time for triggering swap writes and CursorHold
+
+vim.opt.colorcolumn = '80' -- Highlight the 80th column to maintain line length limits
